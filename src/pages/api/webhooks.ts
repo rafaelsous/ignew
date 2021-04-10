1ca30dfe2190dadd3ed243e252607d698f48c4cd
rafaelsous <rafaelsousa.souza@gmail.com>
2021-04-10 11:22:26 -0300

    Salving event data.

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,8 +1,70 @@
-import { NextApiResponse } from "next";
-import { NextApiRequest } from "next-auth/_next";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Readable } from 'stream'
+import Stripe from 'stripe'
 
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  console.log('Evento recebido')
+import { stripe } from '../../services/stripe'
+import saveSubscription from './_lib/manageSubscription'
 
-  return response.status(200).json({ ok: true })
+async function buffer(readable: Readable) {
+  const chunks = []
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    )
+
+    return Buffer.concat(chunks)
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+// A estrutura de dados do tipo Set evita dados repetidos
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+    const buf = await buffer(request)
+    const secret = request.headers['stripe-signature']
+
+    let event: Stripe.Event
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (err) {
+      return response.status(400).send(`Webhook error: ${err.message}`)
+    }
+
+    const { type } = event
+
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+            
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+            )
+            break;
+          default:
+            throw new Error('Unhandled event.')
+        }
+      } catch (err) {
+        return response.json({ error: 'Webhook handler failed.' })
+      }
+    }
+
+    return response.status(200).json({ received: true })
+  } else {
+    response.setHeader('Allow', 'POST')
+    response.status(405).end('Method not allowed')
+  }
 }
